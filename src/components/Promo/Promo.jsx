c970959f4567ecf5b073e3759031ee7d9b88c8f7
Bulat Sitdikov <rdvad2@gmail.com>
2022-12-11 16:30:08 +0300

    add component Promo

---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,29 @@
+import './Promo.css';
+import logo from '../../images/logo.svg';
+import planet from '../../images/planet.svg';
+
+function Promo() {
+  return (
+    <section className='promo'>
+      <div className='promo__container'>
+        <div className='promo__auth-bar'>
+          <img className='promo__logo' src={logo} alt='лого' />
+          <div>
+            <button className='promo__button' type='button'>Регистрация</button>
+            <button className='promo__button promo__button_colored' type='button'>Войти</button>
+          </div>
+        </div>
+        <div className='promo__content'>
+          <div>
+            <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+            <span className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</span>
+          </div>
+          <img className='promo__image' src={planet} alt='' />
+        </div>
+        <button className='promo__button promo__button_bordered' type='button'>Узнать больше</button>
+      </div>
+    </section>
+  );
+}
+
+export default Promo;
