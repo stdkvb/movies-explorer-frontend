c2907ed95d5314b063ac35499a2dd82b432c8083
Bulat Sitdikov <rdvad2@gmail.com>
2022-12-12 22:04:05 +0300

    add router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <>
-      <Main />
-      <Footer />
-    </>
+    <Switch>
+      <Route exact path='/'>
+        <Main />
+        <Footer />
+      </Route>
+      <Route exact path='/movies'>
+        <Movies />
+        <Footer />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,9 @@
+function Movies() {
+  return (
+    <section className='movies'>
+      <div />
+    </section>
+  );
+}
+
+export default Movies;
