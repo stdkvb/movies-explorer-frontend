a30aa4f3660f71f0718be68b32190d5f734f96d2
Bulat Sitdikov <rdvad2@gmail.com>
2022-12-27 19:42:24 +0300

    add component SavedMovies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route exact path='/movies'>
           <Movies />
+        </Route>
+        <Route exact path='/saved-movies'>
+          <SavedMovies />
         </Route>
       </Switch>
       <Footer />
