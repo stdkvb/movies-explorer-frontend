b9e0951ad25c4536e0bb83b6c331f727910b05a0
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-05 23:10:21 +0300

    add validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 
 function Form({
-  title, children, buttonText, formText, link, linkText,
+  title, children, buttonText, formText, link, linkText, isValid, onSubmit,
 }) {
   return (
     <main>
@@ -11,9 +11,9 @@ function Form({
         <div className="form__wrapper">
           <Link to="/" className="logo logo_template"><img src={logo} alt="Логотип" /></Link>
           <h1 className="form__title">{ title }</h1>
-          <form className="form-body">
+          <form className="form-body register__form" onSubmit={onSubmit}>
             {children}
-            <button type="submit" className="form-body__button">{buttonText}</button>
+            <button type="submit" className={`form-body__button ${!isValid ? 'form-body__button_disabled' : ''}`} disabled={!isValid}>{buttonText}</button>
             <p className="form__text">
               {formText}
               <Link to={link} className="navigation-link form__link">{linkText}</Link>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import Form from '../Form/Form';
 
 function Register() {
+  const {
+    register,
+    formState: {
+      errors,
+      isValid,
+    },
+    handleSubmit,
+    setFocus,
+  } = useForm({
+    mode: 'onBlur',
+  });
+
+  useEffect(() => {
+    setFocus('name');
+  }, [setFocus]);
+
   return (
     <Form
       title="Добро пожаловать!"
@@ -9,21 +26,64 @@ function Register() {
       formText="Уже зарегистрированы?"
       link="/sign-in"
       linkText="Войти"
+      isValid={isValid}
+      onSubmit={handleSubmit}
     >
       <label htmlFor="name" className="form-body__label">
         Имя
-        <input type="text" className="form-body__input" name="name" id="name" required />
-        <span className="form-body__error">Что-то пошло не так...</span>
+        <input
+          type="text"
+          className={`form-body__input ${errors.name && 'form-body__input_error'}`}
+          id="name"
+          {...register('name', {
+            required: 'Нужно ввести имя',
+            maxLength: {
+              value: 30,
+              message: 'Максимум 30 символов',
+            },
+            minLength: {
+              value: 2,
+              message: 'Минимум 2 символа',
+            },
+            pattern: {
+              value: /^[а-яА-Яa-zA-ZЁё\-\s]*$/,
+              message: 'Имя может содержать только из символы латиницы, символы кириллицы, пробел и дифис',
+            },
+          })}
+        />
+        <span className="form-body__error">{errors.name && `${errors.name.message || 'Что-то пошло не так...'}`}</span>
       </label>
       <label htmlFor="email" className="form-body__label">
         E-mail
-        <input type="email" className="form-body__input" name="email" id="email" required />
-        <span className="form-body__error">Что-то пошло не так...</span>
+        <input
+          type="email"
+          className={`form-body__input ${errors.email && 'form-body__input_error'}`}
+          id="email"
+          {...register('email', {
+            required: 'Нужно ввести электронную почту',
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: 'Электронная почта не соответствует формату – login@mail.ru',
+            },
+          })}
+        />
+        <span className="form-body__error">{errors.email && `${errors.email.message || 'Что-то пошло не так...'}`}</span>
       </label>
       <label htmlFor="name" className="form-body__label">
         Пароль
-        <input type="password" className="form-body__input" name="password" id="password" required />
-        <span className="form-body__error">Что-то пошло не так...</span>
+        <input
+          type="password"
+          className={`form-body__input ${errors.email && 'form-body__input_error'}`}
+          id="password"
+          {...register('password', {
+            required: 'Нужно ввести пароль',
+            minLength: {
+              value: 6,
+              message: 'Пароль должен быть больше 6 символов',
+            },
+          })}
+        />
+        <span className="form-body__error">{errors.password && `${errors.password.message || 'Что-то пошло не так...'}`}</span>
       </label>
     </Form>
   );
