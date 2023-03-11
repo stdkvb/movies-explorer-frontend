e7e8049861a30dbccfc543952b3049ced9c8ae60
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-11 20:02:05 +0300

    ref profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import HeadMain from '../HeadMain/HeadMain';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import mainApi from '../../utils/MainApi';
 
@@ -52,75 +53,78 @@ function Profile({ onCurrentUser, onLogout }) {
   };
 
   return (
-    <section className='profile'>
-      <div className='profile__container'>
-        <p className='profile__title'>{`Привет, ${name}!`}</p>
-        <form className='profile__form' onSubmit={handleSubmit(handleUpdateSubmit)}>
-          <label className='profile__label' htmlFor='name'>
-            Имя
-            <input
-              className='profile__input'
-              type='text'
-              defaultValue={name || ''}
-              onChange={handleChangeName}
-              disabled={!activeForm}
-              {...register('name', {
-                required: 'Нужно ввести имя',
-                maxLength: {
-                  value: 30,
-                  message: 'В поле «Имя» допустимо максимум 30 символов',
-                },
-                minLength: {
-                  value: 2,
-                  message: 'В поле «Имя» должно быть минимум 2 символа',
-                },
-                pattern: {
-                  value: /^[а-яА-Яa-zA-ZЁё\-\s]*$/,
-                  message: 'Имя может содержать только символы латиницы, символы кириллицы, пробел и дифис',
-                },
-                onChange: handleChangeName,
-                value: newName,
-              })}
-            />
-          </label>
-          <label className='profile__label' htmlFor='email'>
-            Email
-            <input
-              className='profile__input'
-              id='email'
-              defaultValue={email || ''}
-              disabled={!activeForm}
-              {...register('email', {
-                required: 'Нужно ввести электронную почту',
-                pattern: {
-                  value: /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: 'Электронная почта не соответствует формату – login@mail.ru',
-                },
-                onChange: handleChangeEmail,
-                value: newEmail,
-              })}
-            />
-          </label>
-          <button className='profile__button' type='button'>Редактировать</button>
-          <button className='profile__button profile__button_red' type='button'>Выйти из аккаунта</button>
-          {activeForm
-            ? (
-              <>
-                <span className="profile__error">
-                  {(errors.name && errors.name.message) || (errors.email && errors.email.message)}
-                </span>
-                <button type="submit" className={`profile__button ${!isValid ? 'profile__button_disabled' : ''}`} disabled={!isValid}>Сохранить</button>
-              </>
-            )
-            : (
-              <>
-                <button type="button" className="profile__button" onClick={handleActiveForm}>Редактировать</button>
-                <button type="button" className="form-profile__text form-profile__text_important" onClick={onLogout}>Выйти из аккаунта</button>
-              </>
-            )}
-        </form>
-      </div>
-    </section>
+    <>
+      <HeadMain />
+      <section className="form form-profile">
+        <div className="form__wrapper form-profile__wrapper">
+          <h1 className="form__title form-profile__title">{`Привет, ${name}!`}</h1>
+          <form className="form-body register__form" onSubmit={handleSubmit(handleUpdateSubmit)}>
+            <label htmlFor="name" className="form-body__label form-profile__label">
+              Имя
+              <input
+                type="text"
+                className="form-body__input form-profile__input form-profile__input_separate"
+                id="name"
+                defaultValue={name || ''}
+                onChange={handleChangeName}
+                disabled={!activeForm}
+                {...register('name', {
+                  required: 'Нужно ввести имя',
+                  maxLength: {
+                    value: 30,
+                    message: 'В поле «Имя» допустимо максимум 30 символов',
+                  },
+                  minLength: {
+                    value: 2,
+                    message: 'В поле «Имя» должно быть минимум 2 символа',
+                  },
+                  pattern: {
+                    value: /^[а-яА-Яa-zA-ZЁё\-\s]*$/,
+                    message: 'Имя может содержать только символы латиницы, символы кириллицы, пробел и дифис',
+                  },
+                  onChange: handleChangeName,
+                  value: newName,
+                })}
+              />
+            </label>
+            <label htmlFor="email" className="form-body__label form-profile__label">
+              E-mail
+              <input
+                type="email"
+                className="form-body__input form-profile__input"
+                id="email"
+                defaultValue={email || ''}
+                disabled={!activeForm}
+                {...register('email', {
+                  required: 'Нужно ввести электронную почту',
+                  pattern: {
+                    value: /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Электронная почта не соответствует формату – login@mail.ru',
+                  },
+                  onChange: handleChangeEmail,
+                  value: newEmail,
+                })}
+              />
+            </label>
+            {activeForm
+              ? (
+                <>
+                  <span className="form-body__error_submit form-profile__error">
+                    {(errors.name && errors.name.message) || (errors.email && errors.email.message)}
+                  </span>
+                  <button type="submit" className={`form-body__button ${!isValid ? 'form-body__button_disabled' : ''}`} disabled={!isValid}>Сохранить</button>
+                </>
+              )
+              : (
+                <>
+                  <button type="button" className="form-profile__text form-profile__text_separator" onClick={handleActiveForm}>Редактировать</button>
+                  <button type="button" className="form-profile__text form-profile__text_important" onClick={onLogout}>Выйти из аккаунта</button>
+                </>
+              )}
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
