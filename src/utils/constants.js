14c9d51e339d7688a3ff24f57c555806078110dd
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-11 19:24:01 +0300

    change base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
-const BASE_URL = 'localhost:3000';
+const BASE_URL = 'https://api.explorer-movies.nomoredomains.club';
 
 export {
   MOVIES_URL,
