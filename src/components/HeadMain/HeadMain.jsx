14ab4057ac2eb7848936ab4d52bdd3be5aecc708
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-03-11 18:35:13 +0300

    ref headmain

--- src/components/HeadMain/HeadMain.jsx
+++ src/components/HeadMain/HeadMain.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 import logo from '../../images/logo.svg';
 
 function HeadMain() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
-        <meta name="author" content="Sitdikov B." />
-        <link rel="icon" type="image/x-icon" href={logo} />
-        <title>Movies-Explorer</title>
-      </Helmet>
-    </HelmetProvider>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
+      <meta name="author" content="Sitdikov B." />
+      <link rel="icon" type="image/x-icon" href={logo} />
+      <title>Movies-Explorer</title>
+    </Helmet>
   );
 }
 
